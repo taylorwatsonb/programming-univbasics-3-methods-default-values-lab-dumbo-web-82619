83ea027cd1eaf8e72cac7020a9da39b947d9db24
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-01 18:30:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,7 +4,7 @@ def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal
-  return
+  return meal
 end 
 
 meal_choice("kale","quinoa","beef")
